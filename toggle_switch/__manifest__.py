1ed9370e0321c4cc18805acf22a095ba0ba07680
kashif <kashifaziz123@hotmail.com>
2020-12-25 20:59:02 +0500

    Signed-off-by: kashif <kashifaziz123@hotmail.com>

--- toggle_switch/__manifest__.py
+++ toggle_switch/__manifest__.py
@@ -11,7 +11,7 @@
     'price':30,'currency': 'EUR',
     'website': 'https://alhaditech.com',
     'depends': ['base','base_setup','web'],
-    'images': ['static/description/icon.png'],
+    'images': ['static/description/icon.jpeg'],
     'data': [
             "views/assets.xml",
             "views/toggle_switch.xml"
